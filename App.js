59b5b6c19ada1b10f6135f9fd0a694d722d50730
officialskum <95219641+officialskum@users.noreply.github.com>
2022-10-12 10:44:23 +0200

    test

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const andreasVariabel = "hejsan"; 
+
 export default function App() {
   return (
     <View style={styles.container}>
