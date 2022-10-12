e70a66b919b6589416247def5a85a25f6169c4bb
n1ghtb0rn <daniel.falkedal@gmail.com>
2022-10-12 11:58:48 +0200

    added basic nav logic

--- App.js
+++ App.js
@@ -1,15 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Recipes from './components/Recipes.js';
+import Favorite from './components/Favorite.js';
+import Shoplist from './components/Shoplist.js';
+import Pantry from './components/Pantry.js';
+import Profile from './components/Profile.js';
+import { pageStyles } from './styles/styles.js';
+import { useState } from 'react';
+
 //testing more hello
 export default function App() {
+
+  const RECIPES = 0;
+	const FAVORITE = 1;
+	const SHOPLIST = 2;
+  const PANTRY = 3;
+  const PROFILE = 4;
+
+	const [screen, setScreen] = useState(RECIPES);
+
+	let view;
+
+  const changePage = (pageName) => {
+    setScreen(pageName);
+  }
+
+	switch(screen) {
+
+		case RECIPES:
+			view = <Recipes props1={{name: "test", styles: pageStyles}}/>
+			break;
+
+		case FAVORITE:
+			view = <Favorite props1={{name: "test", styles: pageStyles}}/>
+			break;
+
+		case SHOPLIST:
+			view = <Shoplist props1={{name: "test", styles: pageStyles}}/>
+			break;
+
+    case PANTRY:
+			view = <Pantry props1={{name: "test", styles: pageStyles}}/>
+			break;
+
+    case PROFILE:
+			view = <Profile props1={{name: "test", styles: pageStyles}}/>
+			break;
+	}
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Danne was here!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.currentPage}>
+        {view}
+      </View>
+      <View style={styles.navBar}>
+          <Button title="Recipes" onPress={() => {changePage(RECIPES)}}></Button>
+          <Button title="Favorite" onPress={() => {changePage(FAVORITE)}}></Button>
+          <Button title="Shoplist" onPress={() => {changePage(SHOPLIST)}}></Button>
+          <Button title="Pantry" onPress={() => {changePage(PANTRY)}}></Button>
+          <Button title="Profile" onPress={() => {changePage(PROFILE)}}></Button>
+      </View>
+      
     </View>
   );
+  
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,5 +74,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  currentPage: {
+    flex: 1,
+  },
+  navBar: {
+    flexDirection: 'row',
   },
 });
---
+++ styles/styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export const pageStyles = StyleSheet.create({
+	text: {
+		fontSize: 20,
+        color: "blue",
+	},
+});
