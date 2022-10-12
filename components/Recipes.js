85b879d721857ffd8c9daa6a4ed6a11c4c3709cd
LucaSalmi <luca.salmi90@gmail.com>
2022-10-12 16:37:38 +0200

    added flat list to show multiple scrollable cards

--- components/Recipes.js
+++ components/Recipes.js
@@ -1,11 +1,41 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import { useState } from 'react';
 import { Card } from './Card';
 
+
+DATA = [
+    {
+        id: 0,
+        title: 'first',
+    },
+    {
+        id: 1,
+        title: "second",
+    },
+    {
+        id: 3,
+        title: "third",
+    },
+    {
+        id: 4,
+        title: "fourth",
+    },
+]
+
 const Recipes = () => {
 
+    const renderItem = ({ item }) => (
+        <Card myProps={item.title} />
+    );
+
     return (
-        <Card myProps={'text to show'} />
+
+        <FlatList
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            horizontal
+        />
     );
 }
 
