6834828e4add8e9bbb7a7947a3620acbaac1c44b
officialskum <95219641+officialskum@users.noreply.github.com>
2022-10-12 10:54:40 +0200

    commitMessage

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
 //testing more hello
+// a comment from ankan, hello guys
+
 export default function App() {
   return (
     <View style={styles.container}>
