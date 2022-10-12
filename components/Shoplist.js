4dd63d2a2922e8ca1b856b23f4e718999aca4fad
n1ghtb0rn <daniel.falkedal@gmail.com>
2022-10-12 15:51:08 +0200

    update shoplist component

--- components/Shoplist.js
+++ components/Shoplist.js
@@ -1,17 +1,42 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView, Animated } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { danneShoplist } from '../styles/styles.js';
 
 const Shoplist = (props) => {
 
-    
+    const [showSheet, setShowSheet] = useState(false);
 
-    const showPerson = () => {
-        console.log(props.props1.styles.text);
+    const fadeAnim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+
+    const startSheetAnimation = () => {
+        setShowSheet(!showSheet)
+        Animated.timing(
+            fadeAnim,
+            {
+              toValue: 1,
+              duration: 1000,
+              useNativeDriver: true, // <-- Add this
+            }
+          ).start();
     }   
 
 	return (
-        <View>
-            <Text>SHOPLIST PAGE!</Text>
+        <View style={danneShoplist.shoplistContainer}>
+            <View style={danneShoplist.searchBar}></View>
+            <View style={danneShoplist.headerContainer}>
+                <Text style={danneShoplist.headerText}>SHOPLIST PAGE!</Text>
+                <Button style={danneShoplist.filterButton} title="FILTER" onPress={() => { startSheetAnimation() }}></Button>
+            </View>
+
+            <View style={showSheet ? {display: "none"} : danneShoplist.shoppingItemsContainer}>
+                <Text>Item 1</Text>
+                <Text>Item 2</Text>
+                <Text>Item 3</Text>
+            </View>
+            
+            <Animated.View style={showSheet ? danneShoplist.sheetContainer : {display: "none"}}>
+                <Text>SHEET</Text>
+            </Animated.View>
         </View>
         
 	);
