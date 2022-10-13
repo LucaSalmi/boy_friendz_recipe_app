d8935006e6acf756d4fab07735932c9ed33196e3
h0glund <calle.hoglund@student.kyh.se>
2022-10-13 10:39:05 +0200

    material design and searchbar doing stuff

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,11 +1,20 @@
-import React from "react";
-import {View, TextInput, StyleSheet} from 'react-native'
+import {React, useState} from "react";
+import {View, TextInput, StyleSheet, Button, TouchableOpacity} from 'react-native'
+import Icon from 'react-native-ico-material-design';
+
+
 
 const SearchBar = () => {
 
+    const [searchText, setSearchText] = useState("")
+
     return(
         <View style={styles.container}>
-            <TextInput style={styles.searchInput}placeholder="Search here..."/>
+            <TextInput value={searchText} onChangeText={(input) => {setSearchText(input)}} style={styles.searchInput} placeholder="Search here..."/>
+            <TouchableOpacity onPress={() => {setSearchText("")}}>
+                <Icon style={searchText == "" ? {display: "none"} : styles.icon} name="close-button"  height="20" width="20" />
+            </TouchableOpacity>
+            
         </View>
     )
 
@@ -15,18 +24,27 @@ const styles = StyleSheet.create({
     container: {
         display: "flex",
         flexDirection: "row",
-        width: '100%',
+        alignItems: "center",
+        width: '90%',
         height: 50,
         backgroundColor: 'gray',
-        borderRadius: 8 
+        borderRadius: 8,
+        marginTop: 10,
+        
+        
     },
     searchInput: {
-        width: '100%',
+        width: '90%',
         height: '100%',
         paddingLeft: 8,
         fontSize: 16,
         display: "flex",
         justifyContent: "center"
+        
+    },
+    icon: {
+        
+        
         
     }
 })
