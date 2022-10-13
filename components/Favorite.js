a8ae719ef9d924ec70edaae566a424ff5489469b
n1ghtb0rn <daniel.falkedal@gmail.com>
2022-10-13 09:59:48 +0200

    updated Recipes.js and some small fixes

--- components/Favorite.js
+++ components/Favorite.js
@@ -1,19 +1,35 @@
-import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { useState, useEffect } from 'react';
+import { danneFavorite, smallCardStyles } from '../styles/styles.js';
+import SearchBar from './SearchBar.js';
+import { Card } from './Card';
+
+var SingletonInstance = {
+    recipes: [{id: 0, title: "First Card"}, {id: 1, title: "Second Card"}, {id: 2, title: "Third Card"}, {id: 3, title: "Fourth Card"}, {id: 3, title: "Fifth Card"}]
+};
+
 
 const Favorite = (props) => {
 
-    
+    const [recipes, setRecipes] = useState([]);
 
     const showPerson = () => {
         console.log(props.props1.styles.text);
     }   
 
+    useEffect(() => {
+        setTimeout(()=>{
+            setRecipes(SingletonInstance.recipes);
+        }, 500);
+    });
+
 	return (
-        <View>
-            <Text>Hello {props.props1.name}!</Text>
-            <Text style={props.props1.styles.text}>Click Me</Text>
-            <Button title='Log something' onPress={showPerson}></Button>
+        <View style={danneFavorite.favoriteContainer}>
+            <SearchBar />
+            <ScrollView>   
+                {recipes.length == 0 ? <Text style={{paddingTop: 100}}>Loading...</Text> : <Text style={{display: "none"}}>Hidden</Text>}
+                {recipes.map((item, i)=><Card title={item.title} style={smallCardStyles.container} isSmallCard={true} />)}
+            </ScrollView>
         </View>
         
 	);
